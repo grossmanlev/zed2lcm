f848969613e0d1a5ac6e96849a6c635753c3ae67
Lev Grossman <lgrossman@college.harvard.edu>
2018-02-05 15:39:17 -0500

    first commit

---
+++ build/CMakeFiles/zed2lcm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zed2lcm.dir/zed2lcm.cpp.o"
+  "zed2lcm.pdb"
+  "zed2lcm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zed2lcm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ zed2lcm.cpp
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+#include <sl/Camera.hpp>
+#include <lcm/lcm-cpp.hpp>
+
+#include "bot_core/pointcloud2_t.hpp"
+
+using namespace sl;
+
+
+sl::Camera zed;
+sl::Mat cloud;
+lcm::LCM mlcm;
+std::thread zed_callback;
+int width, height;
+
+void startZED();
+void run();
+void close();
+bool quit;
+
+int main (int argc, char** argv)
+{
+	if(!mlcm.good())
+		return 1;
+	
+	bot_core::pointcloud2_t my_data;
+	my_data.height = 640;
+	my_data.width = 480;
+
+
+	Camera zedl;
+    InitParameters init_params;
+	init_params.camera_resolution = RESOLUTION_HD720;
+    init_params.depth_mode = DEPTH_MODE_PERFORMANCE;
+    init_params.coordinate_units = UNIT_METER;
+
+    ERROR_CODE err = zed.open(init_params);
+    if(err != SUCCESS) {
+		zed.close();
+        exit(-1);
+	}
+
+	width = (int) zed.getResolution().width / 2;
+	height = (int) zed.getResolution().height / 2;
+
+	// Start the camera
+	startZED();
+
+
+
+    //printf("Nothing happening...\n");
+
+    //pcl::PointCloud<pcl::PointXYZ> cloud;
+
+    // Fill in the cloud data
+    //cloud.width    = 5;
+    //cloud.height   = 1;
+    //cloud.is_dense = false;
+    //cloud.points.resize (cloud.width * cloud.height);
+
+    //for (size_t i = 0; i < cloud.points.size (); ++i)
+    //{
+    //cloud.points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
+    //cloud.points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
+    //cloud.points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
+    //}
+
+    //pcl::io::savePCDFileASCII ("test_pcd.pcd", cloud);
+    //std::cerr << "Saved " << cloud.points.size () << " data points to test_pcd.pcd." << std::endl;
+
+    //for (size_t i = 0; i < cloud.points.size (); ++i)
+    //std::cerr << "    " << cloud.points[i].x << " " << cloud.points[i].y << " " << cloud.points[i].z << std::endl;
+	
+	while(true) {
+		usleep(1000);
+	}
+    return (0);
+}
+
+void fromPCL(const pcl::PCLPointField &pcl_pf, bot_core::pointfield_t &pf)
+{
+	pf.name = pcl_pf.name;
+	pf.offset = pcl_pf.offset;
+	pf.datatype = pcl_pf.datatype;
+	pf.count = pcl_pf.count;
+}
+
+void fromPCL(const std::vector<pcl::PCLPointField> &pcl_pfs, std::vector<bot_core::pointfield_t> &pfs)
+{
+	pfs.resize(pcl_pfs.size());
+	std::vector<pcl::PCLPointField>::const_iterator it = pcl_pfs.begin();
+	int i = 0;
+	for(; it != pcl_pfs.end(); ++it, ++i) {
+		fromPCL(*(it), pfs[i]);
+	}
+}
+
+
+void startZED() {
+	quit = false;
+	zed_callback = std::thread(run);
+}
+
+void run() {
+	while(!quit) {
+		if(zed.grab() == SUCCESS) {
+			zed.retrieveMeasure(cloud, MEASURE_XYZBGRA, MEM_CPU, width, height);
+			pcl::PointCloud<pcl::PointXYZRGB> point_cloud;
+			point_cloud.width = width;
+			point_cloud.height = height;
+			int size = width*height;
+			point_cloud.points.resize(size);
+
+			sl::Vector4<float>* cpu_cloud = cloud.getPtr<sl::float4>();
+			for(int i = 0; i < size; i++) {
+				point_cloud.points[i].x = cpu_cloud[i][2];
+				point_cloud.points[i].y = -cpu_cloud[i][0];
+				point_cloud.points[i].z = -cpu_cloud[i][1];
+				point_cloud.points[i].rgb = cpu_cloud[i][3];
+			}
+			pcl::PCLPointCloud2 pcl_pc2;
+			pcl::toPCLPointCloud2(point_cloud, pcl_pc2);
+
+			printf("%d, %d\n", pcl_pc2.height, pcl_pc2.width);
+
+
+			bot_core::pointcloud2_t pc2;
+			
+			// header	
+			pc2.seq = pcl_pc2.header.seq;
+			pc2.frame_id = pcl_pc2.header.frame_id;
+			// time????
+
+		
+			pc2.height = pcl_pc2.height;
+			pc2.width = pcl_pc2.width;
+			
+			// fields
+			fromPCL(pcl_pc2.fields, pc2.fields);
+
+
+			pc2.is_bigendian = pcl_pc2.is_bigendian;
+			pc2.point_step = pcl_pc2.point_step;
+			pc2.row_step = pcl_pc2.row_step;
+			pc2.row_step = pcl_pc2.row_step;
+			pc2.data = pcl_pc2.data;
+
+			mlcm.publish("Pointcloud", &pc2);
+
+			
+
+			// pcl::io::savePCDFileASCII ("test_pcd.pcd", point_cloud);
+			// printf("Saved!\n");
+
+			//TODO: Convert the pcl point cloud to PointCloud2 LCM message type
+			
+		}
+		else {
+			sl::sleep_ms(1);		
+		}
+	}
+}
+
+void close() {
+	quit = true;
+	zed_callback.join();
+	cloud.free(MEM_CPU);
+	zed.close();
+}
